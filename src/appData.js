4d9f835f3ba801eb77d1141b144b009812222d64
herokudev <ao43085@gmail.com>
2021-09-09 01:39:40 -0600

    Fixing linter errors
    Fixing linter errors

--- src/appData.js
+++ src/appData.js
@@ -1,20 +1,16 @@
 class AppData {
-    printConsole = () => {
-        console.log("METHOD --> AppData.printConsole");
-    }
-
     addNewScore = (name, score) => {
-        fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zqPTnNutkbrCACFiMaEs/scores/', {
-            method: 'POST',
-            body: JSON.stringify({ 
-            "user": name,
-            "score": score
-          }),
-            headers: {
-              'Content-type': 'application/json; charset=UTF-8',
-            },
-          })
-            .then((response) => response.json())        
+      fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zqPTnNutkbrCACFiMaEs/scores/', {
+        method: 'POST',
+        body: JSON.stringify({
+          user: name,
+          score,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json());
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,45 +1,44 @@
 import './style.css';
 import appdata from './appData';
 
-const scoresList = document.querySelector("#scores-List");
-const addName = document.querySelector("#addName");
-const addScore = document.querySelector("#addScore");
-const btnSubmit = document.querySelector("#btnSubmit");
-const btnRefresh = document.querySelector("#btnRefresh");
+const scoresList = document.querySelector('#scores-List');
+const addName = document.querySelector('#addName');
+const addScore = document.querySelector('#addScore');
+const btnSubmit = document.querySelector('#btnSubmit');
+const btnRefresh = document.querySelector('#btnRefresh');
 
 const addScoreBtn = () => {
-  const row = document.createElement('tr');  
+  const row = document.createElement('tr');
   row.innerHTML = `
     <td>${addName.value}: ${addScore.value} </td>
   `;
   scoresList.appendChild(row);
   appdata.addNewScore(addName.value, addScore.value);
-  addName.value = "";
-  addScore.value = "";    
-}
+  addName.value = '';
+  addScore.value = '';
+};
 
 const addScoresList = () => {
-  let scoresTable = scoresList;
-  while(scoresTable.hasChildNodes())
-  {
+  const scoresTable = scoresList;
+  while (scoresTable.hasChildNodes()) {
     scoresTable.removeChild(scoresTable.firstChild);
   }
   fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zqPTnNutkbrCACFiMaEs/scores/', {
     method: 'GET',
   })
-    .then(response => response.json())
-    .then(data => populateList(data.result));
-}
+    .then((response) => response.json())
+    .then((data) => populateList(data.result));
+};
 
 const populateList = (scores) => {
   scores.forEach((s) => {
-      const row = document.createElement('tr');
-      row.innerHTML = `
+    const row = document.createElement('tr');
+    row.innerHTML = `
       <td>${s.user}: ${s.score} </td>    
       `;
-      scoresList.appendChild(row);
-  });    
-}
+    scoresList.appendChild(row);
+  });
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   addScoresList();
@@ -48,4 +47,3 @@ document.addEventListener('DOMContentLoaded', () => {
 btnSubmit.addEventListener('click', addScoreBtn);
 
 btnRefresh.addEventListener('click', addScoresList);
-
